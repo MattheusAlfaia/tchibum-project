5e35365de3f5c798e78cc7c9dffc556cb238ff21
Mattheus Cruz Alfaia <83054761+MattheusAlfaia@users.noreply.github.com>
2024-08-07 11:52:18 -0400

    Update contatus.blade.php

--- resources/views/contatus.blade.php
+++ resources/views/contatus.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 
-<section class="hero-wrap hero-wrap-2 js-fullheight"  >
+<section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('{{ asset('/storage/'. $contato->imagem_principal) }}');">
     <div class="overlay"></div>
     <div class="container">
       <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
