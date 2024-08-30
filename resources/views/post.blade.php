e33c42b15d3ee5fd5a935813d4cfd6d08875b8f4
Mattheus Cruz Alfaia <83054761+MattheusAlfaia@users.noreply.github.com>
2024-08-30 19:48:37 -0400

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 
-<section class="hero-wrap hero-wrap-1 js-fullheight" style="background-image: url('{{ asset('/storage/13.jpg') }}');">
+<section class="hero-wrap hero-wrap-1 js-fullheight" style="background-image: url('{{ asset('/images/13.jpg') }}');">
   <div class="overlay"></div>
   <div class="container">
     <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
