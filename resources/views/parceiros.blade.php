dda9d603f13eb0cb7fcc6d92af43541dab303358
FlavioSolon <118930194+FlavioSolon@users.noreply.github.com>
2024-09-02 20:13:55 -0400

    Revert "refactory: validate do form de parceiros"
    This reverts commit 876b83450ccfcd683e84162e8cc181cfc82ee920.

--- resources/views/parceiros.blade.php
+++ resources/views/parceiros.blade.php
@@ -49,7 +49,139 @@
     <div class="container">
         <div class="row block-9">
             <div id="block" class="col-md-12 order-md-last d-flex">
-                <livewire:parceiro-form />
+                <form action="{{ route('parceiros.mensagem') }}" method="POST" class="bg-light p-5 contact-form">
+                    @csrf
+
+                    <!-- Informações da Empresa -->
+                    <div class="form-section">
+                        <h3>Informações da Empresa</h3>
+                        <div class="row">
+                            <div class="col-md-6 form-group">
+                                <label>Nome da Empresa</label>
+                                <input name="nome" type="text" class="form-control" required>
+                                @error('nome')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>CNPJ</label>
+                                <input name="cnpj" type="text" class="form-control" placeholder="00.000.000/0000-00">
+                                @error('cnpj')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Endereço</label>
+                                <input name="endereco" type="text" class="form-control" required>
+                                @error('endereco')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>CEP</label>
+                                <input name="cep" type="text" class="form-control" placeholder="00000-000" required>
+                                @error('cep')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Cadastur</label>
+                                <input name="cadastur" type="text" class="form-control">
+                                @error('cadastur')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Seguimento</label>
+                                <select name="seguimento" class="form-control" required>
+                                    <option value="turismo">Turismo</option>
+                                    <option value="hospedagem">Hospedagem</option>
+                                    <option value="guia_turistico">Guia Turístico</option>
+                                    <option value="gastronomia">Gastronomia</option>
+                                    <option value="artesanato">Artesanato</option>
+                                </select>
+                                @error('seguimento')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Informações de Contato -->
+                    <div class="form-section">
+                        <h3>Informações de Contato</h3>
+                        <div class="row">
+                            <div class="col-md-6 form-group">
+                                <label>Seu Email</label>
+                                <input name="email" type="email" class="form-control" required>
+                                @error('email')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Responsável</label>
+                                <input name="responsavel" type="text" class="form-control" required>
+                                @error('responsavel')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Cargo</label>
+                                <input name="cargo" type="text" class="form-control" required>
+                                @error('cargo')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Cidade</label>
+                                <input name="cidade" type="text" class="form-control" required>
+                                @error('cidade')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Comunidade</label>
+                                <input name="comunidade" type="text" class="form-control" required>
+                                @error('comunidade')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label>Número (WhatsApp)</label>
+                                <input name="numero" type="text" class="form-control" required>
+                                @error('numero')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Mensagem -->
+                    <div class="form-section">
+                        <h3>O que te motiva a ser nosso Parceiro?</h3>
+                        <div class="form-group">
+                            <label>Deixe uma breve apresentação</label>
+                            <textarea name="mensagem" class="form-control"></textarea>
+                            @error('mensagem')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <!-- Termos e Envio -->
+                    <div class="form-section">
+                        <div class="form-group">
+                            <input name="accepted_terms" type="checkbox" required>
+                            <label>Termos Aceitos</label>
+                            @error('accepted_terms')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" value="Enviar" class="btn btn-primary py-3 px-5">
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
