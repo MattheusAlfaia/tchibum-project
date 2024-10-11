616916b6423fe1e60faf5f179e8162dc66eb6674
FlavioSolon <118930194+FlavioSolon@users.noreply.github.com>
2024-10-10 21:10:19 -0400

    style: Melhoria na tela de post, melhorando a legibilidade

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,38 +1,34 @@
 @extends('layout')
-@section('title','post')
+@section('title', 'Post')
 @section('content')
 
-
+<!-- Hero Section -->
 <section class="hero-wrap hero-wrap-1 js-fullheight" style="background-image: url('{{ asset('/images/13.jpg') }}');">
-  <div class="overlay"></div>
-  <div class="container">
-    <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
-      <div class="col-md-9 ftco-animate pb-10 text-center">
-       <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="fa fa-chevron-right"></i></a></span> <span>Post <i class="fa fa-chevron-right"></i></span></p>
-       <h1 class="mb-0 bread">{{ $post->titulo  }}</h1>
-       <a href="#posts "> <i  class="fa fa-angle-double-down fa-lg" style="color: white" aria-hidden="true"></i></a>
-     </div>
-   </div>
- </div>
+    <div class="overlay" style="background-color: rgba(0, 0, 0, 0.4);"></div> <!-- Leve sobreposição para melhorar a legibilidade -->
+    <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
+            <div class="col-md-9 ftco-animate pb-10 text-center">
+                <p class="breadcrumbs">
+                    <span class="mr-2">
+                        <a href="/">Home <i class="fa fa-chevron-right"></i></a>
+                    </span>
+                    <span>Post <i class="fa fa-chevron-right"></i></span>
+                </p>
+                <h1 class="mb-0 bread">{{ $post->titulo }}</h1>
+                <a href="#posts">
+                    <i class="fa fa-angle-double-down fa-lg" style="color: white;" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+    </div>
 </section>
 
-
-<section class="ftco-section ftco-about ftco-no-pt img">
-  <div class="container">
-    <div class="row d-flex">
-      <div class="col-md-12 about-intro">
-        <div class="row">
-          <div id="conteudo" class="col-md-6 pl-md-5 py-5">
-            <div class="row justify-content-start pb-3">
-              <div class="col-md-12 heading-section ftco-animate">
-                <!-- <span class="subheading">About Us</span> -->
-                <h2 class="mb-4">{{ $post->titulo }}</h2>
-                <p class="paragrafo">{!!  markdown(nl2br(e($post->conteudo))) !!}</p>
-                <!-- <p><a href="#" class="btn btn-primary">Book Your Destination</a></p> -->
-              </div>
-            </div>
-          </div>
-            <div class="col-md-6 d-flex">
+<!-- Post Content Section -->
+<section class="ftco-section ftco-about ftco-no-pt img" id="posts">
+    <div class="container">
+        <div class="row d-flex">
+            <!-- Imagem Principal (5 colunas no desktop) -->
+            <div class="col-lg-11 col-md-12 d-flex">
                 <div id="img-post" class="col-md-12 d-flex align-items-stretch">
                     <div class="img w-100 align-items-center justify-content-center"
                         style="background-image:url('{{ asset('/storage/' . $post->imagem_principal) }}');">
@@ -40,31 +36,70 @@
                 </div>
             </div>
         </div>
-      </div>
+        <!-- Texto adicional abaixo da imagem (desktop) -->
+        <div class="row">
+            <div class="col-md-12 mt-4">
+                <p class="paragrafo">{!! markdown(nl2br(e($post->conteudo))) !!}</p> <!-- Texto continua abaixo da imagem no desktop -->
+            </div>
+        </div>
     </div>
-  </div>
 </section>
 
-
-
+<!-- Styles -->
 <style>
-
-@media (max-width:759px){
-    #conteudo{
-        margin-top:-250px
+    .hero-wrap {
+        background-size: cover;
+        background-position: center center;
+        background-repeat: no-repeat;
+        position: relative;
     }
-}
- #img-post{
-  height: 550px;
 
- }
+    /* Fullheight class for hero section */
+    .js-fullheight {
+        height: 100vh;
+    }
 
+    /* Responsividade da imagem no post */
+    #img-post {
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        height: 550px; /* Altura fixa para imagem no desktop */
+        border-radius: 8px; /* Opcional: Borda arredondada para destacar a imagem */
+    }
+
+    /* Conteúdo adicional no desktop */
+    .col-md-7 {
+        margin-bottom: -20px;
+    }
+
+    /* No mobile, o conteúdo precisa se ajustar abaixo da imagem */
+    @media (max-width: 759px) {
+        #conteudo {
+            margin-top: 0;
+        }
+
+        #img-post {
+            height: 350px; /* Reduzindo a altura da imagem no mobile */
+        }
+
+        /* A imagem de fundo não deve ocupar 100% da altura no mobile */
+        .hero-wrap {
+            height: auto;
+        }
+
+        /* Ajusta o texto no mobile para não sobrepor a imagem */
+        .paragrafo {
+            font-size: 16px;
+        }
+    }
+
+    /* Responsividade para telas maiores */
+    @media (min-width: 1200px) {
+        #img-post {
+            height: 650px; /* Ajuste de altura para telas grandes */
+        }
+    }
 </style>
 
 @endsection
-
-
-
-
-
-
